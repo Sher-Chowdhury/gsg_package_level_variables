c5dc5ec6e24740cb878922e8b60b7586c1b5831a
Sher Chowdhury <sher.chowdhury@ibm.com>
2019-08-02 15:56:16 +0100

    wip

--- 01_initialised_only.go
+++ 01_initialised_only.go
@@ -1,22 +1,21 @@
 package main
 
 import (
-  "fmt" 
+	"fmt"
 )
 
+func initialisedonly() {
+	// here we declared to variables but we haven't assigned a value to them.
+	// i.e. declared but not yet initialized
+	// by default unitialized int-variables takes the value '0' and
+	// unitialized string-variables takes the value ''.
+	var (
+		name string
+		city string
+		age  int
+	)
 
-func initialised_only() {
-// here we declared to variables but we haven't assigned a value to them. 
-// i.e. declared but not yet initialized
-// by default unitialized int-variables takes the value '0' and
-// unitialized string-variables takes the value ''.
-  var (
-    name string
-    city string
-    age  int
-  )
-
-  fmt.Println("'name' is set to:", name)
-  fmt.Println("'city' is set to:", city)
-  fmt.Println("'age' is set to:", age)
+	fmt.Println("'name' is set to:", name)
+	fmt.Println("'city' is set to:", city)
+	fmt.Println("'age' is set to:", age)
 }
