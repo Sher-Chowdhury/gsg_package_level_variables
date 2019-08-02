0fa252ff651e98fdf0e63c8414ea6634bd76c38e
Sher Chowdhury <sher.chowdhury@ibm.com>
2019-08-02 15:54:41 +0100

    wip

---
+++ 02_initialised_only_alternate_syntax.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+  var name string
+  var city string
+  var age  int
+
+  fmt.Println("'name' is set to:", name)
+  fmt.Println("'city' is set to:", city)
+  fmt.Println("'age' is set to:", age)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Sher-Chowdhury/gsg_package_level_variables.git
+
+go 1.12
