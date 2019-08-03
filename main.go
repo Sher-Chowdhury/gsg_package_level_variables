cd73edc72b0d4ee58100febacab3028d687756d7
Sher Chowdhury <sher.chowdhury@ibm.com>
2019-08-03 09:21:58 +0100

    wip

--- main.go
+++ main.go
@@ -7,25 +7,25 @@ func main() {
 	fmt.Println("EG1 - Initialised only")
 	initialisedonly()
 
-	fmt.Println("EG2 - Initialised only - Alternate syntax")
+	fmt.Println("\nEG2 - Initialised only - Alternate syntax")
 	initialisedonlyAlt1()
 
-	fmt.Println("EG3 - Initialised only - Alternate syntax")
+	fmt.Println("\nEG3 - Initialised only - Alternate syntax")
 	initialisedonlyAlt2()
 
-	fmt.Println("EG4 - Declared Variables")
+	fmt.Println("\nEG4 - Declared Variables")
 	declaredVariables()
 
-	fmt.Println("EG5 - Initialise then declare")
+	fmt.Println("\nEG5 - Initialise then declare")
 	initialiseThenDeclareVariable()
 
-	fmt.Println("EG6 - Declared Variables using type inference")
+	fmt.Println("\nEG6 - Declared Variables using type inference")
 
 	declaredVariablesUsingTypeInference()
 
-	fmt.Println("EG7 - Declared Variables - Alternate syntax")
+	fmt.Println("\nEG7 - Declared Variables - Alternate syntax")
 	declaredVariablesAlt1()
 
-	fmt.Println("EG8 - Check data type")
+	fmt.Println("\nEG8 - Check data type")
 	checkDataType()
 }
