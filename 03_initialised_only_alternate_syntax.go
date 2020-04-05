df903b7dd25ea53600502fea5c1ea1350d9a2132
Sher Chowdhury <sher.chowdhury@ibm.com>
2020-04-05 13:59:27 +0100

    wip

--- 03_initialised_only_alternate_syntax.go
+++ 03_initialised_only_alternate_syntax.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func initialisedonlyAlt2() {
 
+    fmt.Println("##### EG3 output #####")  
+
 	// you can declare multiple variables of the same type on a single line.
 	// although this can make the code harder to read
 	var (
@@ -11,8 +13,8 @@ func initialisedonlyAlt2() {
 		age        int
 	)
 
-    fmt.Println("##### EG3 output #####")
-	fmt.Println("'name' is set to:", name)
-	fmt.Println("'city' is set to:", city)
-	fmt.Println("'age' is set to:", age)
+  
+	fmt.Println("'name' is set to:", name)   // 'name' is set to:
+	fmt.Println("'city' is set to:", city)   // 'city' is set to:
+	fmt.Println("'age' is set to:", age)     // 'age' is set to: 0
 }
